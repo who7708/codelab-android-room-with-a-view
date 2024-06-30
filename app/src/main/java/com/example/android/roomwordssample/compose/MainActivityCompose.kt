50faa0cf36cd1a8f4a01a2abe0660e74cf73e9b4
Chris <who7708@163.com>
2024-06-30 16:50:26 +0800

    compose 页面实现

--- app/src/main/java/com/example/android/roomwordssample/compose/MainActivityCompose.kt
+++ app/src/main/java/com/example/android/roomwordssample/compose/MainActivityCompose.kt
@@ -1,17 +1,37 @@
 package com.example.android.roomwordssample.compose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.android.roomwordssample.R
 import com.example.android.roomwordssample.compose.ui.theme.CodelabandroidroomwithaviewTheme
 
 class MainActivityCompose : ComponentActivity() {
@@ -23,7 +43,10 @@ class MainActivityCompose : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight()
+                            .padding(innerPadding)
                     )
                 }
             }
@@ -34,14 +57,66 @@ class MainActivityCompose : ComponentActivity() {
 @Composable
 fun Greeting(
     name: String,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onValueChange: (String) -> Unit = {
+        Log.i("test", it)
+    },
 ) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 
-    // TextField(value = "Word...", onValueChange = onWordChange)
+    // val udpUiState by udpBroadcastViewModel.uiState.collectAsState()
+    val mediumPadding = dimensionResource(R.dimen.big_padding)
+
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(mediumPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(mediumPadding)
+        ) {
+            OutlinedTextField(
+                // value = "",
+                TextFieldValue(),
+                placeholder = {
+                    "Word..."
+                },
+                singleLine = true,
+                shape = MaterialTheme.shapes.large,
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.surface,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                    disabledContainerColor = MaterialTheme.colorScheme.surface,
+                ),
+                onValueChange = { word ->
+                    Log.i("test", word.toString())
+                },
+                label = { Text("请输入单词") },
+                isError = false,
+                // keyboardType = KeyboardType.Number,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done,
+                    // 限制输入类型
+                    keyboardType = KeyboardType.Text
+                ),
+                // keyboardActions = KeyboardActions(
+                //     onDone = { startUdpListener() }
+                // )
+            )
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { }
+            ) {
+                Text(
+                    text = stringResource(R.string.button_save),
+                    fontSize = 16.sp
+                )
+            }
+        }
+    }
+
+
 }
 
 @Preview(showBackground = true)
